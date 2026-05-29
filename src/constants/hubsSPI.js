e5df78b54a36d53bf60af79be0079bd3b728e21b
Danilo Freiria [SPXBR28072] <danilo.freiria@shopee.com>
2026-05-29 12:28:04 -0300

    ADICIONANDO HUB DE REGISTRO

--- src/constants/hubsSPI.js
+++ src/constants/hubsSPI.js
@@ -29,6 +29,7 @@ export const HUBS_SPI = {
   "LM Hub_SP_Itapeva": "SPI3",
   "LM Hub_SP_Sorocaba_Região Norte": "SPI3",
   "LM Hub_SP_Tatuí": "SPI3",
+  "LM Hub_SP_Registro": "SPI3"
 };
 
 export const LISTA_SPI = Object.keys(HUBS_SPI);
